e7f6832b3798d09981adf5bf5b6d08aafaadffbb
Smriti Singh <smritisingh11a@gmail.com>
2026-06-11 16:09:14 +0530

    ContainsDuplicate.java

---
+++ Arrays & Strings/ContainsDuplicate.java
@@ -0,0 +1,22 @@
+import java.util.Arrays; 
+class ContainsDuplicate{
+    public boolean containsDuplicate(int[]nums){
+        Arrays.sort(nums);
+        int i = 0 ; 
+        int j = 1; 
+        while(j< nums.length){
+            if(nums[i]==nums[j]){
+                return true; 
+            }
+            i++;
+            j--; 
+        }
+        return false; 
+    }
+    public static void main (String[]args){
+        int []nums = {1, 2, 3, 1}; 
+        boolean result = new ContainsDuplicate().containsDuplicate(nums);
+        System.out.println(result);
+
+    }
+}
